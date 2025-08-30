714a6bb164f6f6ded2a37b433c60f7402538d897
Ayush Kumar Singh <ask940555@gmail.com>
2025-08-30 13:29:52 +0530

    Initial commit after cleanup

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+<<<<<<< HEAD
+=======
+// import express from "express";
+// import dotenv from "dotenv";
+// dotenv.config();
+// import connectdb from "./config/db.js";
+// import cookieParser from "cookie-parser";
+// import cors from 'cors'
+// import authRouter from "./routes/auth.routes.js";
+// const app=express();
+// app.use(cors({origin:"https://localhost:5173",
+//   credentials:true
+// }))
+// const port=process.env.PORT || 5000;
+// app.use(express.json())
+// app.use(cookieParser())
+// app.use("/api/auth", authRouter)
+// app.get("/",(req,res)=>{
+//   res.send("hi");
+// })
+// app.listen(port,()=>{
+//   connectdb()
+//   console.log("server connected")
+// }) 
+>>>>>>> e0e4853 (my first commit)
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
@@ -19,4 +44,8 @@ app.get("/",(req,res)=>{
 app.listen(port,()=>{
   connectdb()
   console.log("server connected")
+<<<<<<< HEAD
 })
+=======
+})
+>>>>>>> e0e4853 (my first commit)
