a65d79703bf63fcf57a931a74b11dc8865eebc39
Ayush Kumkar Singh <155828137+aksingh123456@users.noreply.github.com>
2026-01-17 21:30:03 +0530

    Update isAuth.js

--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -1,17 +1,17 @@
-import jwt from "jsonwebtoken"
-const isAuth=async(req,res,next)=>{
-try{
-    const token=req.cookies.token
-    console.log(token);
-    if(!token){
-        return res.status(400).json({msg:"invalid user"});
+import jwt from "jsonwebtoken";
+
+const isAuth = (req,res,next)=>{
+    try{
+        const token = req.cookies.token;
+        if(!token) return res.status(401).json({msg:"User not authenticated"});
+
+        const verified = jwt.verify(token,process.env.JWT_SECRET);
+        req.userId = verified.userId;
+        next();
+    }catch(err){
+        console.log(err);
+        return res.status(401).json({msg:"Token invalid or expired"});
     }
-    const verifytoken =await jwt.verify(token,process.env.JWT_SECRET)
-    req.userId=verifytoken.userId
-   next();
 }
-catch(err){
-    return res.status(400).json({msg:"is auth error"})
-}
-}
-export default isAuth
+
+export default isAuth;
