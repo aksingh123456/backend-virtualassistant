383e36a795abaea7ae30f2c7f0c7fa9d2a50e756
Ayush Kumkar Singh <155828137+aksingh123456@users.noreply.github.com>
2026-01-18 18:27:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,11 +32,9 @@ import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 
 const app = express();
-
-app.use(cors({
-  origin: "https://frontend-virtualassistant.onrender.com",
-  credentials: true
-}));
+ // origin: "https://frontend-virtualassistant.onrender.com",
+  // credentials: true
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
