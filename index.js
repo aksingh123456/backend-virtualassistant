b57319be2a307b561d9dc28a9578542a1cbdcb6b
Ayush Kumkar Singh <155828137+aksingh123456@users.noreply.github.com>
2026-01-18 13:42:01 +0530

    Change CORS origin to localhost for development

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ import userRouter from "./routes/user.routes.js";
 const app = express();
 
 app.use(cors({
-  origin: "https://backend-virtualassistant.vercel.app",
+  origin: "http://localhost:5173",
   credentials: true
 }));
 
